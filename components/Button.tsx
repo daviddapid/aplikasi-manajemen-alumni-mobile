e6f67b08e6debf3892f06355b0eb4ae776c08f5d
daviddapid <daviddapid19@gmail.comm>
2025-05-24 12:25:47 +0700

    update: adjust button text fontweight to be bold

--- components/Button.tsx
+++ components/Button.tsx
@@ -91,5 +91,5 @@ const styles = StyleSheet.create({
 		flexDirection: "row",
 		alignItems: "center",
 	},
-	text: { fontWeight: "semibold", marginHorizontal: "auto" },
+	text: { fontWeight: "bold", marginHorizontal: "auto" },
 });
